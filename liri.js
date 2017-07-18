243a58a79306e7b56e7c01797c83fba0217d402d
ruthbuena <ruthbpeace@gmail.com>
2017-07-18 13:16:16 -0400

    Correct Twitter API npm install

--- liri.js
+++ liri.js
@@ -7,12 +7,29 @@ var request = require('request');
 
 // Part One: Node Last 20 Twitter tweets
 var keys = require('./keys.js');
-var Tweet = require('twitter');
-var myTweets = new Tweet({
+var Twitter = require('twitter');
+var client = new Twitter({
       consumer_key: keys.twitterKeys.consumer_key,
       consumer_secret: keys.twitterKeys.consumer_secret,
       access_token_key: keys.twitterKeys.access_token_key,
       access_token_secret: keys.twitterKeys.access_token_secret
 });
 
-var lastTwenty = {count: 20};
+var params = {count: 20};
+
+function myTweets(){
+  client.get('statuses/user_timeline', params, function(error, tweets,response){
+    if (!error) {
+        for (var i=0; i < tweets.length; i++) {
+          console.log(tweets[i].text + "Created on: " + tweets[i].created_at);
+          fs.appendFile('log.txt', tweets[i].text + "Created on: " + tweets[i].created_at + "\n");
+        }
+          fs.appendFile('log.txt', "============================================================");
+    } else {
+          console.log(error);
+    }
+  });
+}
+
+// Part Two: Node Spotify This Song
+var spotify = require('spotify');
