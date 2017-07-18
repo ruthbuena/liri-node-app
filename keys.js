281de09c8c53ce1024f38e20c273fd574e0d95fa
ruthbuena <ruthbpeace@gmail.com>
2017-07-18 12:44:39 -0400

    Initial file creation

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'S4xMePuOpQ4OnHvTMY1sHFXvA',
+  consumer_secret: 'kgwwQBiyQUcTiLRztdsmH6he0J5bkVJyhYumfBKcBzaSvhTJ5N',
+  access_token_key: '887342781299085312-9F7muNbBeXevK1lEGhHxDRNkfWKRi0x',
+  access_token_secret: '7fLu3LcdxR57sQPxEpSL8EMMdgvLyI1rzx2GfO0m7jIgQ',
+}
